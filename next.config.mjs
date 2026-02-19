d4266b121726e982e30b070a9789331235e77fd9
CNewMedia <christophe@cnip.be>
2026-02-19 23:54:05 +0100

    Build fix - working

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.sanity.io' },
